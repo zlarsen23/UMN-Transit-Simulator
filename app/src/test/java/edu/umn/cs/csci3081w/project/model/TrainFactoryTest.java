c9a7a7c5316a90ac57e7382ad0cfff8c419025aa
matthew.xue2004@gmail.com <matthew.xue2004@gmail.com>
2024-12-05 14:10:07 -0600

    added tests

--- app/src/test/java/edu/umn/cs/csci3081w/project/model/TrainFactoryTest.java
+++ app/src/test/java/edu/umn/cs/csci3081w/project/model/TrainFactoryTest.java
@@ -116,4 +116,48 @@ public class TrainFactoryTest {
     assertEquals(3, trainFactory.getStorageFacility().getDieselTrainsNum());
 
   }
+
+  /**
+   * Testing if vehicle got returned.
+   */
+  @Test
+  public void testReturnVehicleDieselTrain() {
+    List<Stop> stopsIn = new ArrayList<Stop>();
+    Stop stop1 = new Stop(0, "test stop 1", new Position(-93.243774, 44.972392));
+    Stop stop2 = new Stop(1, "test stop 2", new Position(-93.235071, 44.973580));
+    stopsIn.add(stop1);
+    stopsIn.add(stop2);
+    List<Double> distancesIn = new ArrayList<>();
+    distancesIn.add(0.843774422231134);
+    List<Double> probabilitiesIn = new ArrayList<Double>();
+    probabilitiesIn.add(.025);
+    probabilitiesIn.add(0.3);
+    PassengerGenerator generatorIn = new RandomPassengerGenerator(stopsIn, probabilitiesIn);
+
+    Route testRouteIn = new Route(0, "testRouteIn",
+        stopsIn, distancesIn, generatorIn);
+
+    List<Stop> stopsOut = new ArrayList<Stop>();
+    stopsOut.add(stop2);
+    stopsOut.add(stop1);
+    List<Double> distancesOut = new ArrayList<>();
+    distancesOut.add(0.843774422231134);
+    List<Double> probabilitiesOut = new ArrayList<Double>();
+    probabilitiesOut.add(0.3);
+    probabilitiesOut.add(.025);
+    PassengerGenerator generatorOut = new RandomPassengerGenerator(stopsOut, probabilitiesOut);
+
+    Route testRouteOut = new Route(1, "testRouteOut",
+        stopsOut, distancesOut, generatorOut);
+
+    Train testTrain = new DieselTrain(1, new Line(10000, "testLine", "TRAIN",
+        testRouteOut, testRouteIn, new Issue()), 3, 1.0);
+
+    assertEquals(3, trainFactory.getStorageFacility().getElectricTrainsNum());
+    assertEquals(3, trainFactory.getStorageFacility().getDieselTrainsNum());
+    trainFactory.returnVehicle(testTrain);
+    assertEquals(3, trainFactory.getStorageFacility().getElectricTrainsNum());
+    assertEquals(4, trainFactory.getStorageFacility().getDieselTrainsNum());
+
+  }
 }
